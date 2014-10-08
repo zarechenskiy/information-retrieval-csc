fcdf82db8ac72cf5b14d168bdb1600f7f21ba2c6
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2014-10-08 12:02:01 +0400

    Add scripts to run searcher and indexer

--- indexer.sh
+++ indexer.sh
@@ -1,3 +1,4 @@
 docs="$1"
 target="$2"
 
+java -classpath jar:out/production/information-retrieval:lib/annotations ru.csc.ir.index.Indexer $docs $target
---
+++ searcher.sh
@@ -0,0 +1,3 @@
+ind="$1"
+
+java -classpath /home/mikhail/csc/information-retrieval-csc/out/production/information-retrieval:/home/mikhail/csc/information-retrieval-csc/lib/annotations.jar ru.csc.ir.search.Searcher $ind
