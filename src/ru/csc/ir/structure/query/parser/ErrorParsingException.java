58e8b7455a17c25c77b179d039487c276e3010bc
Mikhail Zarechenskiy <rostov93@gmail.com>
2014-10-19 17:37:56 +0400

    Add serial version to ParsingException

--- src/ru/csc/ir/structure/query/parser/ErrorParsingException.java
+++ src/ru/csc/ir/structure/query/parser/ErrorParsingException.java
@@ -1,6 +1,8 @@
 package ru.csc.ir.structure.query.parser;
 
 public class ErrorParsingException extends RuntimeException {
+    private static final long serialVersionUID = 564403076570835304L;
+
     public ErrorParsingException(String errorMessage) {
         super(errorMessage);
     }
